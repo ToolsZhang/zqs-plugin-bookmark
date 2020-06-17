cfd2f7140d04ea62870a8e1ac3fe1b979b188211
ToolsZhang <2369185957@qq.com>
2020-06-17 21:43:19 +0800

    edit

--- src/model.ts
+++ src/model.ts
@@ -2,25 +2,25 @@ import { IModel, Model, Schema } from 'zqs-core/lib/db';
 import { IConfig } from './config';
 
 export function createModel(config: IConfig): IModel {
-    const schema = new Schema(
-        {
-            category: {
-                type: String,
-                enum: config.categories,
-                required: true,
-            },
-            target: {
-                type: String,
-                required: true,
-            },
-        },
-        {
-            timestamps: {},
-        }
-    );
-    return Model({
-        name: '__bookmark',
-        auth: true,
-        schema,
-    });
+  const schema = new Schema(
+    {
+      category: {
+        type: String,
+        enum: config.categories,
+        required: true,
+      },
+      target: {
+        type: String,
+        required: true,
+      },
+    },
+    {
+      timestamps: {},
+    }
+  );
+  return Model({
+    name: '__bookmark',
+    auth: true,
+    schema,
+  });
 }
--- src/setup.ts
+++ src/setup.ts
@@ -5,19 +5,19 @@ import * as moment from 'moment';
 import { setupRouter } from './router';
 
 export const setup = {
-    async pre(app: Zqs): Promise<Router[]> {
-        console.log(
-            `[${colors.green(
-                moment().format('YY-MM-DD HH:mm:ss')
-            )}] Setup plugin ${colors.cyan('bookmark')}`
-        );
-        try {
-            const configPath = app.dir + '/plugins/bookmark';
-            app.config.bookmark = require(configPath)[process.env.NODE_ENV];
-            const routers = await setupRouter(app);
-            return await routers;
-        } catch (e) {
-            console.error(e);
-        }
-    },
+  async pre(app: Zqs): Promise<Router[]> {
+    console.log(
+      `[${colors.green(
+        moment().format('YY-MM-DD HH:mm:ss')
+      )}] Setup plugin ${colors.cyan('bookmark')}`
+    );
+    try {
+      const configPath = app.dir + '/plugins/bookmark';
+      app.config.bookmark = require(configPath)[process.env.NODE_ENV];
+      const routers = await setupRouter(app);
+      return await routers;
+    } catch (e) {
+      console.error(e);
+    }
+  },
 };
